b1bbfb1d67f26c0b681c9c270dee33aa31899e2f
PETRUSBLACK <onukwupetrusoge@gmail.com>
2024-05-03 12:43:34 +0100

    adding the first user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { Authmodule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [Authmodule, UserModule, BookmarkModule],
+  imports: [Authmodule, UserModule, BookmarkModule, PrismaModule],
 })
 export class AppModule {}
